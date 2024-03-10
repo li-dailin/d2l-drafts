c31a59f1c8bf151e2a2cfd1e1b87b88f469e1058
Dailin Li <dailinli2020@mail.ustc.edu.cn>
2024-03-10 17:28:09 +0800

    Initial commit

--- d2l/torch.py
+++ d2l/torch.py
@@ -49,6 +49,10 @@ d2l.DATA_HUB['kaggle_house_test']=(d2l.DATA_URL+'kaggle_house_pred_test.csv','fa
 d2l.DATA_HUB['hotdog']=(d2l.DATA_URL+'hotdog.zip','fba480ffa8aa7e0febbb511d181409f899b9baa5')
 d2l.DATA_HUB['cifar10_tiny']=(d2l.DATA_URL+'kaggle_cifar10_tiny.zip','2068874e4b9a9f0fb07ebe0ad2b29754449ccacd')
 d2l.DATA_HUB['dog_tiny']=(d2l.DATA_URL+'kaggle_dog_tiny.zip','0cb91d09b814ecdc07b50f31f8dcad3e81d6a86d')
+d2l.DATA_HUB['voc2012']=(d2l.DATA_URL+'VOCtrainval_11-May-2012.tar','4e443f8a2eca6b1dac8a6c57641b67dd40621a49')
+
+VOC_COLORMAP=[[0,0,0],[128,0,0],[0,128,0],[128,128,0],[0,0,128],[128,0,128],[0,128,128],[128,128,128],[64,0,0],[192,0,0],[64,128,0],[192,128,0],[64,0,128],[192,0,128],[64,128,128],[192,128,128],[0,64,0],[128,64,0],[0,192,0],[128,192,0],[0,64,128]]
+VOC_CLASSES=['background','aeroplane','bicycle','bird','boat','bottle','bus','car','cat','chair','cow','diningtable','dog','horse','motorbike','person','potted plant','sheep','sofa','train','tv/monitor']
 
 
 # Part 01: 图像绘制与实用类
@@ -567,3 +571,67 @@ def reorg_test(data_dir):
     """在预测期间整理测试集,以方便读取"""
     for test_file in os.listdir(os.path.join(data_dir,'test')):
         copyfile(os.path.join(data_dir,'test',test_file),os.path.join(data_dir,'train_valid_test','test','unknown'))
+
+def read_voc_images(voc_dir,is_train=True):
+    """读取所有VOC图像并标注"""
+    txt_fname=os.path.join(voc_dir,'ImageSets','Segmentation','train.txt' if is_train else 'val.txt')
+    mode=torchvision.io.image.ImageReadMode.RGB
+    with open(txt_fname,'r') as f:
+        images=f.read().split()
+    features,labels=[],[]
+    for i,fname in enumerate(images):
+        features.append(torchvision.io.read_image(os.path.join(voc_dir,'JPEGImages',f'{fname}.jpg')))
+        labels.append(torchvision.io.read_image(os.path.join(voc_dir,'SegmentationClass',f'{fname}.png'),mode))
+    return features,labels
+
+def voc_colormap2label():
+    """构建从RGB到VOC类别索引的映射"""
+    colormap2label=torch.zeros(256**3,dtype=torch.long)
+    for i,colormap in enumerate(VOC_COLORMAP):
+        colormap2label[(colormap[0]*256+colormap[1])*256+colormap[2]]=i
+    return colormap2label
+
+def voc_label_indices(colormap,colormap2label):
+    """将VOC标签中的RGB值映射到它们的类别索引"""
+    colormap=colormap.permute(1,2,0).numpy().astype('int32')
+    idx=((colormap[:,:,0]*256+colormap[:,:,1])*256+colormap[:,:,2])
+    return colormap2label[idx]
+
+def voc_rand_crop(feature,label,height,width):
+    """随机裁剪特征和标签图像"""
+    rect=torchvision.transforms.RandomCrop.get_params(feature,(height,width))
+    feature=torchvision.transforms.functional.crop(feature,*rect)
+    label=torchvision.transforms.functional.crop(label,*rect)
+    return feature,label
+
+class VOCSegDataSet(torch.utils.data.Dataset):
+    """一个y用于加载VOC数据集的自定义数据集"""
+    def __init__(self,is_train,crop_size,voc_dir):
+        self.transform=torchvision.transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])
+        self.crop_size=crop_size
+        features,labels=read_voc_images(voc_dir,is_train=is_train)
+        self.features=[self.normalize_image(feature) for feature in self.filter(features)]
+        self.labels=self.filter(labels)
+        self.colormap2label=voc_colormap2label()
+        print('read '+str(len(self.features))+' examples')
+
+    def normalize_image(self,img):
+        return self.transform(img.float()/255)
+
+    def filter(self,imgs):  # 移除尺寸小于裁剪尺寸的图像
+        return [img for img in imgs if (img.shape[1]>=self.crop_size[0] and img.shape[2]>=self.crop_size[1])]
+        
+    def __getitem__(self,idx):
+        feature,label=voc_rand_crop(self.features[idx],self.labels[idx],*self.crop_size)
+        return (feature,voc_label_indices(label,self.colormap2label))
+
+    def __len__(self):
+        return len(self.features)
+
+def load_data_voc(batch_size,crop_size):
+    """加载VOC语义分割数据集"""
+    voc_dir=d2l.download_extract('voc2012',os.path.join('VOCdevkit','VOC2012'))
+    num_workers=d2l.get_dataloader_workers()
+    train_iter=torch.utils.data.DataLoader(VOCSegDataSet(True,crop_size,voc_dir),batch_size,shuffle=True,drop_last=True,num_workers=num_workers)
+    test_iter=torch.utils.data.DataLoader(VOCSegDataSet(False,crop_size,voc_dir),batch_size,drop_last=True,num_workers=num_workers)
+    return train_iter,test_iter
